f7eeafe1643195f0106b48b0e1db5e8e38664890
Rabin Chakraborty <110186169+RabinChakraborty@users.noreply.github.com>
2023-04-27 11:12:02 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export const Footer = () => {
     <footer class='relative text-center text-black-400 lg:text-center md:text-center sm:text-center'>
       <div className='my-4'>
         <h3 className='footer-content font-semibold text-gray-300'>
-          By Rabin Chakraborty Student of AlmaBetter.
+          By Rabin Chakraborty 
         </h3>
       </div>
     </footer>
